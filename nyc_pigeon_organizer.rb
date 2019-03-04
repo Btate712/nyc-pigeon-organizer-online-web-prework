bccded771f5b2d5cb0d446ac096a097f1371b570
Robert Tate <btate712@gmail.com>
2019-03-04 02:11:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,13 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+  pigion_list = {}
   data.each do |category, data_level_2|
     data_level_2.each do |category_level_2, pigions|
-      binding.pry
+      pigions.each do |pigion|
+        pigion_list[pigion] = {}
+      end
     end
   end
-
+  puts pigion_list
 end
